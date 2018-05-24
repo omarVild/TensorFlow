f4072330d04bb123be8e66612f89b64817ac236f
Omar Villalpando <g_omar_v@live.com.mx>
2018-05-23 19:29:52 -0500

    Hello with placeholders
    Hello with placeholders

--- TensorFlowBasics/basics/HelloWorldTensorFlow.py
+++ TensorFlowBasics/basics/HelloWorldTensorFlow.py
@@ -7,9 +7,18 @@ world = tf.constant("world")
 
 zeros =tf.zeros((4,4))
 
+helloPH = tf.placeholder(tf.string, name='hello')
+namePH = tf.placeholder(tf.string, name='name')
+
+addOpr = helloPH + namePH
+
 
 with tf.Session() as sess:
     res = sess.run(hello+world)
+    print(res)
+    res2 = sess.run(addOpr, feed_dict={helloPH:'Hello ', namePH:'Omar'}  )
+    print(res2)
 
     
-print(res)
+
+
